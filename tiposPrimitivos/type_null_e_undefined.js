41da5bfc468cda381fbdb8b32e765e3b2f810478
unknown <lucascostabomfim5@gmail.com>
2022-02-21 23:51:51 -0300

    exemplos de null e undefined

---
+++ tiposPrimitivos/type_null_e_undefined.js
@@ -0,0 +1,18 @@
+// exemplo de null
+
+let input = null;
+
+if (input === null) {
+ console.log('não há informação');
+} else {
+ console.log(input);
+}
+
+// exemplo de undefined
+
+let input = null;
+let input2;
+
+console.log(input); // null
+console.log(input2); // undefined
+
