295fd965a03459c6b101c8452d409b0f5dccf217
gri0t <lucascostabomfim5@gmail.com>
2022-04-06 01:13:01 -0300

    calculando media com for e foreach

---
+++ arrays/media_foreach.js
@@ -0,0 +1,12 @@
+const notas = [10, 6.5, 8, 7.5]
+
+let somaNotas = 0
+
+// callback
+notas.forEach(nota => {
+    somaNotas += nota
+})
+
+let media = somaNotas / notas.length
+
+console.log(media)
